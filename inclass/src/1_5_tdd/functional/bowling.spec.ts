46990d379a9dd4829025812501eb54a2a1679538
Jorge Alberto Chí León <jorch.aleon@gmail.com>
2022-09-08 13:56:44 -0500

    Tdd del funcional Tarea 5

--- inclass/src/1_5_tdd/functional/bowling.spec.ts
+++ inclass/src/1_5_tdd/functional/bowling.spec.ts
@@ -1,6 +1,58 @@
+import {createGame, Game} from "./bowling"
 
 describe('A bowling game', () => {
-    
+    let game: Game
+    beforeEach(() => {
+        game = createGame()
+    })
+
+    it('can roll a ball', () =>{
+        game = game.roll(0)
+    })
+
+    it('can play a gutter game', () =>{
+        game = rollMany(game, 20, 0)
+
+        expect(game.score()).toBe(0)
+    })
+
+    it('can roll a game with all ones', () =>{
+        game = rollMany(game, 20, 1)
+
+        expect(game.score()).toBe(20)
+    })
+
+    it('can roll a spare', () =>{
+        game = game.roll(5)
+        game.roll(5)
+        game.roll(3)
+
+        game = rollMany(game, 17, 0)
+
+        expect(game.score()).toBe(16)
+    })
+
+    it('can roll a strike', () =>{
+        game = game.roll(10)
+        game.roll(1)
+        game.roll(1)
+
+        game = rollMany(game, 16, 0)
+
+        expect(game.score()).toBe(14)
+    })
+
+    it('can roll a PERFECT GAME', () =>{
+        game = rollMany(game, 12, 10)
+
+        expect(game.score()).toBe(300)
+    })
 
 });
 
+function rollMany(game: Game, rolls: number, pins: number){
+    for(let i = 0; rolls > i; i++){
+        game = game.roll(pins)
+    }
+    return game
+}
--- inclass/src/1_5_tdd/functional/bowling.ts
+++ inclass/src/1_5_tdd/functional/bowling.ts
@@ -1,1 +1,34 @@
 //the code
+type State = {rolls: number}
+
+const initialState = {rolls:[]}
+
+export const createGame = (State: State = initialState) => Object.freeze({
+    roll: (pins: number) => roll(state,pins),
+    score:() => score(state)
+})
+
+const roll = (State: State, pins: number) => {
+    const rolls = [...state.rolls, pins]
+    return createGame({rolls})
+}
+
+const score = ({rolls}: State) => {
+    let score = 0
+    let firstInFrame = 0
+    for (let frame = 0; 10>frame; frmae++ ){
+        if(rolls[firstInFrame] === 10){f
+            score += 10 + rolls[firstInFrame+1] + rolls[firstInFrame+2]
+            firstInFrame++
+        }else if(rolls[firstInFrame] + rolls[firstInFrame+1] === 10){
+            score += 10 + rolls[firstInFrame+2]
+            firstInFrame+= 2
+        } else {
+            score += rolls[firstInFrame] + rolls[firstInFrame+1]
+            firstInFrame+= 2
+        }
+    }
+    return score
+}
+
+export type Game = ReturnType<typeof createGame>
